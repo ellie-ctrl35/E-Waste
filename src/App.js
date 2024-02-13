1c573ac014330ba91fa513a15e139413810a9cf6
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-13 00:40:01 +0000

    Add routing and update UserHome styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'; 
 import './App.css';
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import UserHome from './pages/Home/UserHome';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<UserHome />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+            </Routes>
+        </BrowserRouter>
   );
 }
 
--- src/pages/Home/UserHome.jsx
+++ src/pages/Home/UserHome.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import'../../App.css';
 function UserHome(props) {
     return (
-        <div>
+        <div className='App'>
             
         </div>
     );
