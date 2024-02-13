c91008183b8162af1e2e374585f197ddbea5f06d
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-13 01:03:33 +0000

    Add Google Maps API Loader to UserHome component

--- src/pages/Home/UserHome.jsx
+++ src/pages/Home/UserHome.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import'../../App.css';
+import {useJsApiLoader,GoogleMap} from '@react-google-maps/api'
 function UserHome() {
+
+    const {isLoaded} = useJsApiLoader({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+    })
+    if (!isLoaded) {
+        return <div>Loading...</div>
+    }
+
     return (
         <div className='App'>
             <div className='Mapbox'>
-
+              <GoogleMap>
+                
+              </GoogleMap>
             </div>
         </div>
     );
