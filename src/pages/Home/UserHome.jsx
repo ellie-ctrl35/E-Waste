26bbf11906b8358002f3ad1ef04af7b681a306fb
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-13 02:29:39 +0000

    Update dependencies and styles

--- src/pages/Home/UserHome.jsx
+++ src/pages/Home/UserHome.jsx
@@ -4,30 +4,30 @@ import {useJsApiLoader,GoogleMap} from '@react-google-maps/api';
 import axios from 'axios';
 
 const center = {
-    lat: 37.7749,
-    lng: -122.4194
+    lat:  8.00000000,
+    lng: -2.00000000
 }
 
 function UserHome() {
-    const [username, setUsername] = useState('someUsername'); // replace with actual username logic
-    const [number, setNumber] = useState('1234567890');       // replace with actual number logic
-    const [location, setLocation] = useState({ lat: null, lng: null, name: null });
+    //const [username, setUsername] = useState('someUsername'); // replace with actual username logic
+   // const [number, setNumber] = useState('1234567890');       // replace with actual number logic
+  //  const [location, setLocation] = useState({ lat: null, lng: null, name: null });
 
 
     const loaderOptions = useMemo(() => ({
         googleMapsApiKey: "AIzaSyB_oFQ3l8sdvksjPmf-q5lK75YPv0N2Kp4"
-        // Add other consistent options here if necessary
+       
     }), []);
 
-    const handleMapClick = (event) => {
-        setLocation({
-            lat: event.latLng.lat(),
-            lng: event.latLng.lng(),
-            name: 'Place Name' // Replace this with actual place name retrieval logic if available
-        });
-    };
+   // const handleMapClick = (event) => {
+    //    setLocation({
+    //        lat: event.latLng.lat(),
+      //      lng: event.latLng.lng(),
+      //      name: 'Place Name' // Replace this with actual place name retrieval logic if available
+    //    });
+   // };
 
-    const sendLocationData = () => {
+   {/* const sendLocationData = () => {
         const data = {
             username,
             number,
@@ -41,7 +41,7 @@ function UserHome() {
             .catch(error => {
                 console.error('Error:', error);
             });
-    };
+    };*/}
 
     const { isLoaded } = useJsApiLoader(loaderOptions);
     if (!isLoaded) {
@@ -51,14 +51,14 @@ function UserHome() {
     return (
         <div className='App'>
             <div className='Mapbox'>
-              <GoogleMap
+             <GoogleMap
                zoom={10} center={center} mapContainerStyle={{width:"100%",height:"100%"}}
-               onDblClick={handleMapClick}
+             
                >
 
-              </GoogleMap>
+    </GoogleMap>
             </div>
-            <button onClick={sendLocationData}>Send Location</button>
+            <button className='location-btn'>Send Location</button>
         </div>
     );
 }
