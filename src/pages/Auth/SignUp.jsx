98b15bfd278375a021984fdb84a4b9a5a9ebc0eb
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-16 01:52:27 +0000

    Add login form to SignUp page

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './Auth.css'
+import { Link } from 'react-router-dom';
 
 function SignUp() {
     return (
-        <div>
-            
+        <div className='container'>
+            <div className='auth-container'>
+                <h1 className='logo'>
+                    EcoHaul
+                </h1>
+               <div className='form-container'>
+                <form>
+                    <h1>Login to EcoHaul</h1>
+                    <label>Email</label>
+                    <input type='text' placeholder='hello@gmail.com'/>
+                    <label>Password</label>
+                    <input type='password' placeholder='Your Password'/>
+                    <Link className='forgot-pwd'>Forgot Password?</Link>
+                    <button type='submit'>Log in</button>
+                    
+                </form>
+               </div>
+            </div>
         </div>
     );
 }
