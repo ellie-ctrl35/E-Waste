c94f576d6a990ff95a4882ad7c7b3269ec92a6c6
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-16 01:17:27 +0000

    Add login form and styling to Auth page

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -5,7 +5,18 @@ function Login() {
     return (
         <div className='container'>
             <div className='auth-container'>
-
+                <h1 className='logo'>
+                    EcoHaul
+                </h1>
+               <div className='form-container'>
+                <form>
+                    <h1>Login to EcoHaul</h1>
+                    <input type='text' placeholder='hello@gmail.com'/>
+                    <input type='password' placeholder='Password'/>
+                    <button type='submit'>Login</button>
+                    <p>Don't have an account? <a href='/signup'>Sign Up</a></p>
+                </form>
+               </div>
             </div>
         </div>
     );
