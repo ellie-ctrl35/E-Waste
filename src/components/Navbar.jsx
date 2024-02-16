510adddf12e83d42551f5e19390628e8922c62df
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-16 03:40:55 +0000

    Add logo and navigation items to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,20 @@ import './Navbar.css'
 const  Navbar = (props) =>{
     return (
         <div className='navbar'>
-          nabbar
+            <div className='logo'>
+                EcoHaul
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className='logout-container'>
+               
+            </div>
         </div>
     );
 }
