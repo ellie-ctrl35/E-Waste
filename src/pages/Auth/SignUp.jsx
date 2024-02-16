c7adb89878343f684d1871e930be044e4b7afa45
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-16 02:11:00 +0000

    Update phone input type to accept phone number format

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import {useState} from 'react';
 import './Auth.css'
 import { Link } from 'react-router-dom';
 
 function SignUp() {
+    const [email,setEmail]= useState("");
+    const [name,setName]= useState("");
+    const [password,setPassword]=useState("");
+    const navigate = useNavigate();
     return (
         <div className='container'>
             <div className='auth-container'>
@@ -15,7 +19,7 @@ function SignUp() {
                     <label>Name</label>
                     <input type='text' placeholder='Emmanuel Nyatepe'/>
                     <label>Phone</label>
-                    <input type='number' placeholder='hello@gmail.com'/>
+                    <input type='tel' pattern='[0-9]{3}-[0-9]{4}-[0-9]{3}' placeholder='023-4554-678'/>
                     <label>Email</label>
                     <input type='text' placeholder='hello@gmail.com'/>
                     <label>Password</label>
