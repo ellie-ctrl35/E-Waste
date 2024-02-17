9c062fe3de0a3e84e8a0ed1c347fb214c16228ac
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-17 00:54:56 +0000

    Commented out unused import statements

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SignUp from './pages/Auth/SignUp';
 import UserHome from './pages/UserPages/UserHome';
 import AdminHome from './pages/Admin/AdminHome';
 import User from './pages/UserPages/User';
-import { useContext } from 'react';
+import { createContext } from 'react';
 
 export const UserContext = createContext();
 
