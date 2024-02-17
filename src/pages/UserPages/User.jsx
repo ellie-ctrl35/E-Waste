6071f9ff3cf24245fd4347da009fa33a64d627d7
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-02-17 12:12:59 +0000

    Update User.css and User.jsx

--- src/pages/UserPages/User.jsx
+++ src/pages/UserPages/User.jsx
@@ -1,13 +1,33 @@
 import Navbar from '../../components/Navbar';
-import './User.css';
+import'../../App.css';
+import Avatar from 'react-avatar';
+import searchIcon from '../../resources/searchIcon.png';
+import notification from '../../resources/notification.png';
+import { UserContext } from '../../App';
+import {useState,useContext} from 'react';
+import axios from 'axios';
 
 function User() {
+    const user = useContext(UserContext);
+    const user_id = user.user_id;
+    const username = user.username;
     return (
-        <div className='body'>
+        <div className='App'>
             <Navbar />
-            
             <div className='right-side'>
-                
+                <div className='top-div'>
+                    <div className='search-container'>
+                        <img src={searchIcon} alt="search" />
+                        <input type="text" placeholder="Search for a location" />
+                    </div>
+                    <div className='icongroup'>
+                        <img src={notification} alt='notification'/>
+                        <Avatar name={username} size="40" round={true} />
+                    </div>
+                </div>
+                <div className='mid-div'>
+                    
+                </div>
             </div>
         </div>
     );
